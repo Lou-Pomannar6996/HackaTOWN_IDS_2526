004ae022136970771131e98370920a584a8113dd
Polenta Alessandro <alessandro.polenta@studenti.unicam.it>
2026-01-19 20:53:06 +0100

    Merge con progetto creato

---
+++ src/main/java/it/ids/hackathown/HackathownApplication.java
@@ -0,0 +1,13 @@
+package it.ids.hackathown;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackathownApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackathownApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/ids/hackathown/HackathownApplicationTests.java
@@ -0,0 +1,13 @@
+package it.ids.hackathown;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackathownApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
